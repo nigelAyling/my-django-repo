616b5958adc6362dedba21da050ac3979208c2df
nigelAyling <nigel.w.ayling@gmail.com>
2021-11-17 19:27:10 +0000

    Video 15 done

--- my_django_site/articles/urls.py
+++ my_django_site/articles/urls.py
@@ -2,8 +2,10 @@
 from django.urls import path, re_path
 from.import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list),
-    re_path(r'^(?P<slug>[\w-]+)/$',views.article_detail),
+    path('', views.article_list,name="list"),
+    re_path(r'^(?P<slug>[\w-]+)/$',views.article_detail,name="detail"),
 
 ]
