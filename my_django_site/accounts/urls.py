d85361690c3cd10f8c68ffbb24a221a080dbf26a
nigelAyling <nigel.w.ayling@gmail.com>
2021-11-24 20:45:26 +0000

    videos 20,21 and 22 done

--- my_django_site/accounts/urls.py
+++ my_django_site/accounts/urls.py
@@ -5,5 +5,6 @@ app_name='accounts'
 
 urlpatterns = [
     path('signup/', views.signup_view,name="signup"),
+    path('login/', views.login_view,name="login"),
     #re_path(r'^(?P<slug>[\w-]+)/$',views.article_detail,name="detail"),
 ]
